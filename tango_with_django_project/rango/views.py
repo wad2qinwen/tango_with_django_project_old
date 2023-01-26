483a13eeaadd55123d7c493c51455df95840b995
Qinwen <2721817y@student.gla.ac.uk>
2023-01-26 19:04:56 +0000

    'update'

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    #construct a dictionary to pass to template as its content
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context = context_dict)
+
+def about(request):
+     context_dict = {'boldmessage': 'This tutorial has been put together by qinwen'}
+     return render(request, 'rango/about.html', context = context_dict)
